15d91fbca6ac62b2b5dab35525e2006aa3443502
hoangdmh <dmhoang08t1@gmail.com>
2023-11-13 11:19:46 +0700

    Setup Redux - config store

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import './style/global.scss';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    user: [
+        { id: 1, name: 'Hoang' },
+        { id: 2, name: 'Hoang Nam' }
+    ]
+}
+
+const rootReducerReducer = (state = initState, action) => {
+
+    return state;
+}
+
+export default rootReducerReducer
